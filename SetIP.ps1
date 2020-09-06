37c712e07515611764b833a54540d48391b0532d
Rasmus Dyrberg Hansen <rasm033d@student.eadania.dk>
2020-09-06 16:58:30 +0000

    Updated SetIP.ps1

--- SetIP.ps1
+++ SetIP.ps1
@@ -1,8 +1,11 @@
 #Setting the IP for Windows manually
 $ipParams = @{
+#choose the network interface card to change values for
 InterfaceIndex = 8
+#set the IP address
 IPAddress = "192.168.2.50"
 PrefixLength = 24
 AddressFamily = "IPv4"
 }
+#Assign new IP address
 New-NetIPAddress @ipParams
